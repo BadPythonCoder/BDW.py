6be3a090ea3c25810fa7fdbc33d12aa848cbf4d5
BadPythonCoder <66245823+BadPythonCoder@users.noreply.github.com>
2021-11-20 09:08:44 +0000

    rekt the stealers, jk

--- BDW/bdw/__init__.py
+++ BDW/bdw/__init__.py
@@ -1,5 +1,8 @@
 '''
-A bad discord wrapper made by Fox551/BadPythonProgrammer.
+A bad discord wrapper made by Fox551/BadPythonProgrammer and TBSharedAccount.
+
+for everybody that wants to steal this for some weird reason, yeah no this is our... thing. Just gonna slap some symbols on it
+©©©©©©20212021202120212021 by Fox551!!!! CHECK DATE OF REPL AND GITHUB FOR PROOV D:<
 '''
 from .Guild import *
 from .Bot import *
